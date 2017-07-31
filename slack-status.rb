32ee33c313a09d4e13b54a1c07e24176ee1305cc
goreleaserbot <goreleaser@carlosbecker.com>
2017-07-31 22:15:02 +0900

    slack-status version v1.1.0

--- slack-status.rb
+++ slack-status.rb
@@ -1,9 +1,9 @@
 class SlackStatus < Formula
   desc ""
   homepage ""
-  url "https://github.com/townewgokgok/slack-status/releases/download/1.0.1/slack-status_1.0.1_darwin_amd64.tar.gz"
-  version "1.0.1"
-  sha256 "747a6240da9a124e227af3896de7f340718127b643d43d62200c33261770df77"
+  url "https://github.com/townewgokgok/slack-status/releases/download/v1.1.0/slack-status_1.1.0_darwin_amd64.tar.gz"
+  version "1.1.0"
+  sha256 "9ff18efa6614010f023dcb8330f1760fedc7b3cdb4be290a5405f54641dea6b9"
 
   def install
     bin.install "slack-status"
